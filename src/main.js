c2055e1e5f56da769cbf5966392a91c184b85e85
zhouyan <zhouyanshengwei@163.com>
2020-03-12 10:19:00 +0800

    新增sass基础文件

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import '@/assets/styles/index.scss'
 Vue.config.productionTip = false
-/* eslint-disable */
+    /* eslint-disable */
 
 Vue.use(ElementUI)
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
